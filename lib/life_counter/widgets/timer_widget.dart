241715f8b7266010b8ad354f435e0305af3a7b1e
Joshua Shewmaker <jshewmak@gmail.com>
2024-12-10 20:30:02 -0500

    added pause

--- lib/life_counter/widgets/timer_widget.dart
+++ lib/life_counter/widgets/timer_widget.dart
@@ -18,13 +18,24 @@ class TimerWidget extends StatelessWidget {
                 context.read<GameBloc>().state.status == GameStatus.paused
                     ? context.read<GameBloc>().add(const GameResumeEvent())
                     : context.read<GameBloc>().add(const GamePauseEvent()),
-            child: Text(
-              '${(elapsedSeconds ~/ 60).toString().padLeft(2, '0')}:${(elapsedSeconds % 60).toString().padLeft(2, '0')}',
-              style: const TextStyle(
-                color: AppColors.neutral60,
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
-              ),
+            child: Row(
+              children: [
+                if (context.watch<GameBloc>().state.status == GameStatus.paused)
+                  const Icon(
+                    Icons.pause,
+                    color: AppColors.neutral60,
+                    size: 40,
+                  )
+                else
+                  Text(
+                    '${(elapsedSeconds ~/ 60).toString().padLeft(2, '0')}:${(elapsedSeconds % 60).toString().padLeft(2, '0')}',
+                    style: const TextStyle(
+                      color: AppColors.neutral60,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+              ],
             ),
           );
         },
