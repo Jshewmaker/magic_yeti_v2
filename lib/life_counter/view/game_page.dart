42936f73ee0c822ccda5ca4612fc2f22ab0f8ea9
Joshua Shewmaker <jshewmak@gmail.com>
2025-01-15 10:25:52 -0500

    updated dialog

--- lib/life_counter/view/game_page.dart
+++ lib/life_counter/view/game_page.dart
@@ -55,119 +55,174 @@ class _GameOverDialogState extends State<_GameOverDialog> {
   Widget build(BuildContext context) {
     final players = context.watch<GameBloc>().state.playerList;
 
-    return AlertDialog(
-      title: const Text('Game Over'),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          SwitchListTile(
-            title: const Text('Turn 1 Sol Ring'),
-            value: turnOneSolRing,
-            onChanged: (bool value) {
-              setState(() {
-                turnOneSolRing = value;
-              });
-            },
+    return Dialog(
+      alignment: Alignment.center,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.80,
+        height: MediaQuery.of(context).size.height * 0.80,
+        child: AlertDialog(
+          actionsPadding: const EdgeInsets.only(top: 128.0),
+          alignment: Alignment.topCenter,
+          title: Center(
+            child: Text(
+              'Game Over',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
           ),
-          if (players.isNotEmpty) ...[
-            const SizedBox(height: 16),
-            Row(
+          content: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Text('Who went first: '),
-                const SizedBox(width: 8),
                 Expanded(
-                  child: DropdownButtonFormField<String>(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                    ),
-                    value: firstPlayerId,
-                    items: players.map((player) {
-                      return DropdownMenuItem(
-                        value: player.id,
-                        child: Text(player.name),
-                      );
-                    }).toList(),
-                    onChanged: (String? value) {
-                      setState(() {
-                        firstPlayerId = value;
-                      });
-                    },
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // SwitchListTile(
+                      //   title: Text(
+                      //     'Turn 1 Sol Ring',
+                      //     style: Theme.of(context).textTheme.titleLarge,
+                      //   ),
+                      //   value: turnOneSolRing,
+                      //   onChanged: (bool value) {
+                      //     setState(() {
+                      //       turnOneSolRing = value;
+                      //     });
+                      //   },
+                      // ),
+                      if (players.isNotEmpty) ...[
+                        const SizedBox(height: 32),
+                        Text(
+                          'Who went first:',
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        const SizedBox(height: 8),
+                        DropdownButtonFormField<String>(
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 16),
+                          ),
+                          value: firstPlayerId,
+                          items: players.map((player) {
+                            return DropdownMenuItem(
+                              value: player.id,
+                              child: Text(
+                                player.name,
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (String? value) {
+                            setState(() {
+                              firstPlayerId = value;
+                            });
+                          },
+                        ),
+                        const SizedBox(height: 32),
+                        Text(
+                          'Please select account owner:',
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        const SizedBox(height: 8),
+                        DropdownButtonFormField<String>(
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 16, vertical: 16),
+                          ),
+                          value: selectedPlayerId,
+                          items: players.map((player) {
+                            return DropdownMenuItem(
+                              value: player.id,
+                              child: Text(
+                                player.name,
+                                style: Theme.of(context).textTheme.titleMedium,
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (String? value) {
+                            setState(() {
+                              selectedPlayerId = value;
+                            });
+                          },
+                        ),
+                      ],
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                const Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisSize: MainAxisSize.min,
                   ),
                 ),
               ],
             ),
-            const SizedBox(height: 16),
+          ),
+          actions: [
             Row(
               children: [
-                const Text('Please select account owner: '),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: DropdownButtonFormField<String>(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                    ),
-                    value: selectedPlayerId,
-                    items: players.map((player) {
-                      return DropdownMenuItem(
-                        value: player.id,
-                        child: Text(player.name),
-                      );
-                    }).toList(),
-                    onChanged: (String? value) {
-                      setState(() {
-                        selectedPlayerId = value;
-                      });
-                    },
+                TextButton(
+                  onPressed: () {
+                    context.read<GameBloc>().add(const GameResumeEvent());
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    'Cancel',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ),
+                const Spacer(),
+                ElevatedButton(
+                  onPressed: selectedPlayerId == null && firstPlayerId == null
+                      ? null
+                      : () {
+                          context.read<GameBloc>().add(
+                                GameUpdatePlayerOwnershipEvent(
+                                  playerId: selectedPlayerId!,
+                                  firebaseId:
+                                      context.read<AppBloc>().state.user.id,
+                                  firstPlayerId: firstPlayerId ?? '',
+                                ),
+                              );
+
+                          Navigator.of(context).pop();
+                        },
+                  child: Text(
+                    'Play Again',
+                    // style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ),
+                const SizedBox(width: 16),
+                ElevatedButton(
+                  onPressed: selectedPlayerId == null && firstPlayerId == null
+                      ? null
+                      : () {
+                          context.read<GameBloc>().add(
+                                GameUpdatePlayerOwnershipEvent(
+                                  playerId: selectedPlayerId!,
+                                  firebaseId:
+                                      context.read<AppBloc>().state.user.id,
+                                  firstPlayerId: firstPlayerId ?? '',
+                                ),
+                              );
+                          GoRouter.of(context).go(HomePage.routeName);
+                          Navigator.of(context).pop();
+                        },
+                  child: Text(
+                    'Return to Home',
+                    //  style: Theme.of(context).textTheme.titleMedium,
                   ),
                 ),
               ],
             ),
           ],
-        ],
+        ),
       ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            context.read<GameBloc>().add(const GameResumeEvent());
-            Navigator.of(context).pop();
-          },
-          child: const Text('Cancel'),
-        ),
-        const Spacer(),
-        ElevatedButton(
-          onPressed: selectedPlayerId == null && firstPlayerId == null
-              ? null
-              : () {
-                  context.read<GameBloc>().add(
-                        GameUpdatePlayerOwnershipEvent(
-                          playerId: selectedPlayerId!,
-                          firebaseId: context.read<AppBloc>().state.user.id,
-                          firstPlayerId: firstPlayerId ?? '',
-                        ),
-                      );
-
-                  Navigator.of(context).pop();
-                },
-          child: const Text('Play Again'),
-        ),
-        const SizedBox(width: 8),
-        ElevatedButton(
-          onPressed: selectedPlayerId == null && firstPlayerId == null
-              ? null
-              : () {
-                  context.read<GameBloc>().add(
-                        GameUpdatePlayerOwnershipEvent(
-                          playerId: selectedPlayerId!,
-                          firebaseId: context.read<AppBloc>().state.user.id,
-                          firstPlayerId: firstPlayerId ?? '',
-                        ),
-                      );
-                  GoRouter.of(context).go(HomePage.routeName);
-                  Navigator.of(context).pop();
-                },
-          child: const Text('Return to Home'),
-        ),
-      ],
     );
   }
 }
