b08b3aa263aba77a6c7aa5b6f5534014152ee580
Joshua Shewmaker <jshewmak@gmail.com>
2025-01-25 23:13:29 -0500

    formatting

--- lib/profile/view/profile_page.dart
+++ lib/profile/view/profile_page.dart
@@ -23,45 +23,54 @@ class ProfilePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            if (user.imageUrl != null && user.imageUrl!.isNotEmpty)
-              Center(
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundImage: NetworkImage(user.imageUrl!),
-                ),
-              ),
-            const SizedBox(height: 20),
-            _ProfileField(label: 'Username', value: user.username ?? 'Not set'),
-            _ProfileField(
-              label: 'First Name',
-              value: user.firstName ?? 'Not set',
-            ),
-            _ProfileField(
-              label: 'Last Name',
-              value: user.lastName ?? 'Not set',
-            ),
-            _ProfileField(label: 'Email', value: user.email ?? 'Not set'),
-            _ProfileField(label: 'Bio', value: user.bio ?? 'Not set'),
-            const SizedBox(height: 20),
-            Center(
-              child: Row(
+            SizedBox(
+              width: 500,
+              child: Column(
                 children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      // TODO: Add edit profile functionality
-                    },
-                    child: const Text('Edit Profile'),
+                  if (user.imageUrl != null && user.imageUrl!.isNotEmpty)
+                    Center(
+                      child: CircleAvatar(
+                        radius: 75,
+                        backgroundImage: NetworkImage(user.imageUrl!),
+                      ),
+                    ),
+                  const SizedBox(height: 50),
+                  _ProfileField(
+                      label: 'Username', value: user.username ?? 'Not set'),
+                  _ProfileField(
+                    label: 'First Name',
+                    value: user.firstName ?? 'Not set',
                   ),
-                  const SizedBox(width: 16),
-                  ElevatedButton(
-                    onPressed: () {
-                      context.read<AppBloc>().add(const AppLogoutRequested());
-                      context.pop();
-                    },
-                    child: const Text('Sign Out'),
+                  _ProfileField(
+                    label: 'Last Name',
+                    value: user.lastName ?? 'Not set',
+                  ),
+                  _ProfileField(label: 'Email', value: user.email ?? 'Not set'),
+                  _ProfileField(label: 'Bio', value: user.bio ?? 'Not set'),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          // TODO: Add edit profile functionality
+                        },
+                        child: const Text('Edit Profile'),
+                      ),
+                      const SizedBox(width: 16),
+                      ElevatedButton(
+                        onPressed: () {
+                          context
+                              .read<AppBloc>()
+                              .add(const AppLogoutRequested());
+                          context.pop();
+                        },
+                        child: const Text('Sign Out'),
+                      ),
+                    ],
                   ),
                 ],
               ),
@@ -86,17 +95,17 @@ class _ProfileField extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 8),
-      child: Column(
+      child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            label,
-            style: Theme.of(context).textTheme.titleSmall,
+            '$label:',
+            style: Theme.of(context).textTheme.titleLarge,
           ),
-          const SizedBox(height: 4),
+          const SizedBox(width: 8),
           Text(
             value,
-            style: Theme.of(context).textTheme.bodyLarge,
+            style: Theme.of(context).textTheme.titleLarge,
           ),
         ],
       ),
