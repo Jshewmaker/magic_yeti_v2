c3556550e702f63852e582f8ed0cfc5bbc7db6a2
Joshua Shewmaker <jshewmak@gmail.com>
2026-07-19 09:55:54 -0400

    feat: filter friend head-to-head stats by time range

--- lib/friends_list/friend_stats/friend_stats_state.dart
+++ lib/friends_list/friend_stats/friend_stats_state.dart
@@ -12,12 +12,13 @@ final class FriendStatsInitial extends FriendStatsState {}
 final class FriendStatsLoading extends FriendStatsState {}
 
 final class FriendStatsLoaded extends FriendStatsState {
-  const FriendStatsLoaded(this.stats);
+  const FriendStatsLoaded(this.stats, {this.range = StatsTimeRange.allTime});
 
   final FriendHeadToHead stats;
+  final StatsTimeRange range;
 
   @override
-  List<Object?> get props => [stats];
+  List<Object?> get props => [stats, range];
 }
 
 final class FriendStatsFailure extends FriendStatsState {
